029d236e671d7156037809561d64629cfd6f9d15
abdulhai <abdulhai.elegant@gmail.com>
2025-06-30 06:26:48 +0500

    added coverage and dist in gitignore

--- dist/lib/api/health.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Health = (ctx) => {
-    ctx.body = { status: 'ok' };
-};
-exports.default = Health;
--- dist/lib/schemas/issue.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateIssueSchema = exports.CreateIssueSchema = void 0;
-const zod_1 = require("zod");
-exports.CreateIssueSchema = zod_1.z.object({
-    title: zod_1.z.string().min(1),
-    description: zod_1.z.string().min(1),
-});
-exports.UpdateIssueSchema = exports.CreateIssueSchema.partial();
